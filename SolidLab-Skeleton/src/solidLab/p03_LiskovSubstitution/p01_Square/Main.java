50e31bb33d8aa881b5151a3489f15d2bf9d136c5
SophYo <sophiya.dimitrova@gmail.com>
2019-07-22 23:27:06 +0300

    code added

--- SolidLab-Skeleton/src/solidLab/p03_LiskovSubstitution/p01_Square/Main.java
+++ SolidLab-Skeleton/src/solidLab/p03_LiskovSubstitution/p01_Square/Main.java
@@ -2,6 +2,18 @@ package solidLab.p03_LiskovSubstitution.p01_Square;
 
 public class Main {
     public static void main(String[] args) {
+
+        Square square = new Square();
+
+        Rectangle rectangle = new Rectangle();
+
+        printArea(square);
+        printArea(rectangle);
+
         System.out.println("Hello World");
+    }
+
+    private static void printArea(Shape shape) {
+        System.out.println(shape.getArea());
     }
 }
