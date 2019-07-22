ef1a8592de6e6fae7574267dfee8a67ebcc3f4a0
SophYo <sophiya.dimitrova@gmail.com>
2019-07-22 22:16:02 +0300

    deleted

--- SolidLab-Skeleton/src/solidLab/p01_SingleResponsibility/p01_DrawingShape/DrawingManagerImpl.java
+++
@@ -1,6 +0,0 @@
-package solidLab.p01_SingleResponsibility.p01_DrawingShape;
-
-import solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.Renderer;
-import solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.Shape;
-
-
--- SolidLab-Skeleton/src/solidLab/p01_SingleResponsibility/p01_DrawingShape/interfaces/DrawingRepository.java
+++
@@ -1,2 +0,0 @@
-package solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces;
-
