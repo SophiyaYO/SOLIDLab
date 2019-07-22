4a81e96aaac7d0b2206bfed52b2c5d1f8a7f6057
SophYo <sophiya.dimitrova@gmail.com>
2019-07-22 22:16:16 +0300

    added code test code

--- SolidLab-Skeleton/src/solidLab/p01_SingleResponsibility/p01_DrawingShape/Main.java
+++ SolidLab-Skeleton/src/solidLab/p01_SingleResponsibility/p01_DrawingShape/Main.java
@@ -1,7 +1,15 @@
 package solidLab.p01_SingleResponsibility.p01_DrawingShape;
 
+import solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.Renderer;
+import solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.RendererImpl;
+
 public class Main {
     public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 7);
+
+        Renderer renderer = new RendererImpl();
+
+        rectangle.draw(renderer);
         System.out.println("Hello World");
     }
 }
